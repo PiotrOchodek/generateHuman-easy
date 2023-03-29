21b2732ac23c86f75124954e40b289c9ce22bc51
Piotr Ochodek <ohis@op.pl>
2023-03-29 13:53:28 +0200

    I added class GenerateHuman

--- script.js
+++ script.js
@@ -12,8 +12,23 @@ generateHuman.prototype.changePhoneNumber = function (newPhoneNumber) {
     this.phoneNumber = newPhoneNumber
 }
 
-const peter = new generateHuman('Piotr', 'Ochodek', 35, '500532607', 'Poland', '#1')
-console.log(peter)
+const peter = new GenerateHuman('Piotr', 'Ochodek', 35, '500532607', 'Poland', '#1')
 
 peter.changePhoneNumber(500400300)
 console.log(peter)
+
+class GenerateHuman {
+    constructor(name, surname, age, phoneNumber, country, id) {
+    this.name = name,
+    this.surname = surname,
+    this.email = `${name}${surname}@test.pl`
+    this.age = age,
+    this.phoneNumber = phoneNumber,
+    this.country = country,
+    this.id = id
+    }
+}
+
+const monica = new GenerateHuman('Monica', 'Jones', 32, '123123123', 'Poland', '#2')
+
+console.log(monica)
