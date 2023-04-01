e5829b22a0110f4526c250e175ecf2ddaeed49e2
Piotr Ochodek <ohis@op.pl>
2023-04-01 14:16:53 +0200

    rebased with master and and facing an error

--- script.js
+++ script.js
@@ -12,23 +12,26 @@ generateHuman.prototype.changePhoneNumber = function (newPhoneNumber) {
     this.phoneNumber = newPhoneNumber
 }
 
-const peter = new GenerateHuman('Piotr', 'Ochodek', 35, '500532607', 'Poland', '#1')
+const peter = new generateHuman('Piotr', 'Ochodek', 35, '500532607', 'Poland', '#1')
+console.log(peter)
 
 peter.changePhoneNumber(500400300)
 console.log(peter)
 
-class GenerateHuman {
-    constructor(name, surname, age, phoneNumber, country, id) {
-    this.name = name,
-    this.surname = surname,
-    this.email = `${name}${surname}@test.pl`
-    this.age = age,
-    this.phoneNumber = phoneNumber,
-    this.country = country,
-    this.id = id
-    }
-}
 
-const monica = new GenerateHuman('Monica', 'Jones', 32, '123123123', 'Poland', '#2')
 
-console.log(monica)
+// class GenerateHuman {
+//     constructor(name, surname, age, phoneNumber, country, id) {
+//     this.name = name,
+//     this.surname = surname,
+//     this.email = `${name}${surname}@test.pl`
+//     this.age = age,
+//     this.phoneNumber = phoneNumber,
+//     this.country = country,
+//     this.id = id
+//     }
+// }
+
+// const monica = new GenerateHuman('Monica', 'Jones', 32, '123123123', 'Poland', '#2')
+
+// console.log(monica)
