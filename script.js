e23747be9b9488e7d3c0f63a0956057dd1c821e9
Piotr Ochodek <ohis@op.pl>
2023-04-01 15:50:59 +0200

    Class GenerateHuman with 2 methods inside

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-function generateHuman(name, surname, age, phoneNumber, country, id) {
-  (this.name = name),
-    (this.surname = surname),
-    (this.email = `${name}${surname}@test.pl`);
-  (this.age = age),
-    (this.phoneNumber = phoneNumber),
-    (this.country = country),
-    (this.id = id);
-}
-const dataHistory = [];
-const addNumberToHistory = (newPhoneNumber) => {
-  dataHistory.push({ old_phone_number: newPhoneNumber });
-};
-generateHuman.prototype.changePhoneNumber = function (newPhoneNumber) {
-  const oldNumber = this.phoneNumber;
-  this.phoneNumber = newPhoneNumber;
-  return addNumberToHistory(oldNumber);
-};
-
-const peter = new generateHuman(
-  'Piotr',
-  'Ochodek',
-  35,
-  '500532607',
-  'Poland',
-  '#1'
-);
-console.log(peter);
-
-peter.changePhoneNumber(500400300);
-peter.changePhoneNumber(100200300);
-peter.changePhoneNumber(999999999);
-console.log(peter);
-console.log(dataHistory);
-
-// Class
 class GenerateHuman {
   constructor(name, surname, age, phoneNumber, country, id) {
     (this.name = name),
@@ -44,15 +8,29 @@ class GenerateHuman {
       (this.country = country),
       (this.id = id);
   }
+  addNumberToHistory = (newPhoneNumber) => {
+    const dataHistory = [];
+    dataHistory.push({ old_phone_number: newPhoneNumber });
+    console.log(dataHistory)
+    return dataHistory
+  };
+  changePhoneNumber = (newPhoneNumber) => {
+    const oldNumber = this.phoneNumber;
+    this.phoneNumber = newPhoneNumber;
+    return this.addNumberToHistory(oldNumber);
+  };
 }
 
-const monica = new GenerateHuman(
-  'Monica',
-  'Jones',
-  32,
-  '123123123',
-  'Poland',
-  '#2'
-);
 
-console.log(monica);
+const peter = new GenerateHuman(
+  'Piotr',
+  'Ochodek',
+  35,
+  '500532607',
+  'Poland',
+  '#1'
+);
+console.log(peter);
+peter.changePhoneNumber('111222333')
+peter.changePhoneNumber('555666777')
+console.log(peter)
