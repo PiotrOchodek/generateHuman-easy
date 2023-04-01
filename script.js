5067557e2fdb494b424e421bc5c5c4e7e9408bc5
Piotr Ochodek <ohis@op.pl>
2023-04-01 14:25:40 +0200

    Check method that change phone number

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-function generateHuman (name, surname, age, phoneNumber, country, id) { 
-    this.name = name,
-    this.surname = surname,
-    this.email = `${name}${surname}@test.pl`
-    this.age = age,
-    this.phoneNumber = phoneNumber,
-    this.country = country,
-    this.id = id
+function generateHuman(name, surname, age, phoneNumber, country, id) {
+  (this.name = name),
+    (this.surname = surname),
+    (this.email = `${name}${surname}@test.pl`);
+  (this.age = age),
+    (this.phoneNumber = phoneNumber),
+    (this.country = country),
+    (this.id = id);
 }
 generateHuman.prototype.changePhoneNumber = function (newPhoneNumber) {
-    this.oldPhoneNumber = Number(this.phoneNumber)
-    this.phoneNumber = newPhoneNumber
-}
+  this.oldPhoneNumber = Number(this.phoneNumber);
+  this.phoneNumber = newPhoneNumber;
+};
 
-const peter = new generateHuman('Piotr', 'Ochodek', 35, '500532607', 'Poland', '#1')
+const peter = new generateHuman(
+  'Piotr',
+  'Ochodek',
+  35,
+  '500532607',
+  'Poland',
+  '#1'
+);
+console.log(peter);
 
-peter.changePhoneNumber(500400300)
-console.log(peter)
+peter.changePhoneNumber(500400300);
+console.log(peter);
 
 class GenerateHuman {
-    constructor(name, surname, age, phoneNumber, country, id) {
-    this.name = name,
-    this.surname = surname,
-    this.email = `${name}${surname}@test.pl`
-    this.age = age,
-    this.phoneNumber = phoneNumber,
-    this.country = country,
-    this.id = id
-    }
+  constructor(name, surname, age, phoneNumber, country, id) {
+    (this.name = name),
+      (this.surname = surname),
+      (this.email = `${name}${surname}@test.pl`);
+    (this.age = age),
+      (this.phoneNumber = phoneNumber),
+      (this.country = country),
+      (this.id = id);
+  }
 }
 
-const monica = new GenerateHuman('Monica', 'Jones', 32, '123123123', 'Poland', '#2')
+const monica = new GenerateHuman(
+  'Monica',
+  'Jones',
+  32,
+  '123123123',
+  'Poland',
+  '#2'
+);
 
-console.log(monica)
+console.log(monica);
