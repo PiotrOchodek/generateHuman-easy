1b371d5e0f2d2ae991935358ef773f7c6395aec2
Piotr Ochodek <ohis@op.pl>
2023-04-02 11:25:15 +0200

    Class user that creates a person

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-class GenerateHuman {
-  constructor(name, surname, age, phoneNumber, country, id) {
-    (this.name = name),
-      (this.surname = surname),
-      (this.email = `${name}${surname}@test.pl`);
-    (this.age = age),
-      (this.phoneNumber = phoneNumber),
-      (this.country = country),
-      (this.id = id);
+class User {
+  constructor(name, surname, age, phoneNumber, country) {
+    this.name = name,
+    this.surname = surname,
+    this.email = `${name}${surname}@test.pl`;
+    this.age = age,
+    this.phoneNumber = phoneNumber,
+    this.country = country,
+    this.currentHistoryRecordID = 1;
+    this.historyList = [];
   }
-  addNumberToHistory = (oldNumber) => {
-    this.history = [`old phone number: ${oldNumber}`];
-  };
-  changePhoneNumber = (newPhoneNumber) => {
-    const oldNumber = this.phoneNumber;
+  incrementCounter() {
+    this.currentHistoryRecordID = this.currentHistoryRecordID + 1;
+  }
+  createRecordData() {
+    return {
+      ID: this.currentHistoryRecordID,
+      name: 'Previous phone number',
+      number: this.phoneNumber,
+      revision_date: new Date().toLocaleString(),
+    };
+  }
+  addRecordToHistory() {
+    const recordData = this.createRecordData();
+    this.historyList.push(recordData);
+    this.incrementCounter();
+  }
+  setPhoneNumber(newPhoneNumber) {
+    this.addRecordToHistory();
     this.phoneNumber = newPhoneNumber;
-    return this.addNumberToHistory(oldNumber);
-  };
+  }
 }
 
-const peter = new GenerateHuman(
-  'Piotr',
-  'Ochodek',
-  35,
-  '500532607',
-  'Poland',
-  '#1'
-);
+const peter = new User('Piotr', 'Ochodek', 35, '500532607', 'Poland');
 console.log(peter);
-peter.changePhoneNumber('111222333');
+peter.setPhoneNumber('111222333');
+peter.setPhoneNumber('555666777');
+console.log(peter.historyList);
